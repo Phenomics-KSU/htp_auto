256d590022989b5e3aff360c96bebf2efa04bc4a
kmcgahee <kmcgahee@ksu.edu>
2015-06-14 00:20:37 -0500

    Updated to use TCP for sensor control.

--- src/sensor_control_client.py
+++ src/sensor_control_client.py
@@ -22,49 +22,67 @@ def odom_callback(odom):
     pitch = euler[1]
     yaw = euler[2]
     for client in clients:
-        client.send_time_position_and_orientation(utc_time, x, y, z, roll, pitch, yaw)
+        try:
+            client.send_time_position_and_orientation(utc_time, x, y, z, roll, pitch, yaw)
+        except socket.error:
+            rospy.logerr('Socket error: client could not send time/position information. Trying to reconnect to {0}'.format(client.address))
+            try:
+                client.reconnect()
+            except socket.error, e:
+                rospy.logerr('Error reconnecting: {0}'.format(e))
 
 class SensorControlClient:
-    '''Basic UDP client that allows client to send time, position and commands to sensors.
-    
-        There is no connect() call since UDP has no connections.'''
+    '''Basic TCP client that allows client to send time, position and commands to sensors.'''
     
     def __init__(self, host, port):
-        # SOCK_DGRAM is the socket type to use for UDP sockets
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # SOCK_STREAM is the socket type to use for TCP sockets
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.address = (host, port)
-
+        
+    def connect(self):
+        '''Connect to server.'''
+        self.sock.connect(self.address)
+    
+    def reconnect(self):
+        '''Create new socket (cannot reuse old one) and connect to it.'''
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect()
+    
+    def close(self):
+        '''Disconnect from server.'''
+        self.sock.close()
+        
+    def is_connected(self):
+        '''Return true if connected.  Check by trying to send data over connection.'''
+        try:
+            self.sock.send('?')
+            return True
+        except socket.error:
+            return False
+    
     def send_time(self, time):
         '''Send time over socket.'''
-        self.sock.sendto("t,{0}\n".format(time), self.address)
-        
-    def send_position(self, x, y, z):
-        '''Send 3D position over socket.'''
-        self.sock.sendto("p,{0},{1},{2}\n".format(x, y, z), self.address)
-        
-    def send_orientation(self, angle1, angle2, angle3):
-        '''Send 3D orientation over socket.'''
-        self.sock.sendto("o,{0},{1},{2}\n".format(angle1, angle2, angle3), self.address)
+        self.sock.sendall("<t,{0}>".format(time))
         
     def send_time_and_position(self, time, x, y, z):
         '''Send both time and position at same time.'''
-        self.sock.sendto("tp,{0},{1},{2},{3}\n".format(time, x, y, z), self.address)
+        self.sock.sendall("<tp,{0},{1},{2},{3}>".format(time, x, y, z))
         
     def send_time_position_and_orientation(self, time, x, y, z, angle1, angle2, angle3):
         '''Send time, position and orientation at same time.'''
-        self.sock.sendto("tpo,{0},{1},{2},{3},{4},{5},{6}\n".format(time, x, y, z, angle1, angle2, angle3), self.address)
+        self.sock.sendall("<tpo,{0},{1},{2},{3},{4},{5},{6}>".format(time, x, y, z, angle1, angle2, angle3))
         
     def send_command_by_type(self, sensor_type, command):
         '''Send command to all sensors of specified type.'''
-        self.sock.sendto("ct,{0},{1}\n".format(sensor_type, command), self.address)
+        self.sock.sendall("<ct,{0},{1}>".format(sensor_type, command))
         
     def send_command_by_name(self, sensor_name, command):
         '''Send command to all sensors with matching name (names aren't unique)'''
-        self.sock.sendto("cn,{0},{1}\n".format(sensor_name, command), self.address)
+        self.sock.sendall("<cn,{0},{1}>".format(sensor_name, command))
         
     def send_command_by_id(self, sensor_id, command):
         '''Send command to sensor with matching ID. All ID's are unique.'''
-        self.sock.sendto("ci,{0},{1}\n".format(sensor_id, command), self.address)
+        self.sock.sendall("<ci,{0},{1}>".format(sensor_id, command))
 
 if __name__ == '__main__':
     
@@ -106,6 +124,10 @@ if __name__ == '__main__':
         port = int(host[1])
         rospy.loginfo('Connecting to server at {0}:{1}'.format(host_name, port))
         client = SensorControlClient(host_name, port)
+        try:
+            client.connect()
+        except socket.error, e:
+            rospy.logerr('Error connecting to server: {0}'.format(e))
         clients.append(client)
         
     rospy.Subscriber("gps_utc", OdometryUTC, odom_callback)
